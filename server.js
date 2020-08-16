fdebac1273767b55a6ac4e411c84912919f6f629
saifoelloh <saifoelloh@gmail.com>
2020-08-16 22:21:55 +0700

    minor update

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-var methodOverride = require("method-override")
-const path = require("path")
-const con = require("./config/db.js")
+var methodOverride = require('method-override')
+const path = require('path')
+const con = require('./config/db.js')
+require('dotenv').config()
+
+const port = process.env.PORT
 
 // Using pug template engine
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "pug")
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // connecting route to database
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   req.con = con
   next()
 })
@@ -17,17 +20,20 @@ app.use(function(req, res, next) {
 // parsing body request
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+app.use(methodOverride('_method'))
 
 // include router
-const biodataRouter = require("./routes/biodataRouter")
-const orderRouter = require("./routes/orderRouter")
+const biodataRouter = require('./routes/biodataRouter')
+const orderRouter = require('./routes/orderRouter')
+const apiOrder = require('./api/order/route')
 
 // routing
-app.use("/biodata", biodataRouter)
-app.use("/order", orderRouter)
+app
+  .use('/biodata', biodataRouter)
+  .use('/order', orderRouter)
+  .use('/foo', apiOrder)
 
 // starting server
-app.listen(5000, function() {
-  console.log("server listening on port 5000")
+app.listen(port, function () {
+  console.log(`server listening on port ${port}`)
 })
