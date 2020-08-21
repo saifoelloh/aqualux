8566af4ea25c13993dfa6ecd630f17ef889b762d
rstx32 <rstx32@gmail.com>
2020-08-22 04:42:02 +0700

    add create query

--- api/order/model.js
+++ api/order/model.js
@@ -2,4 +2,21 @@ module.exports = {
   get: function (con, callback) {
     con.query('SELECT * FROM `order`', callback)
   },
+
+  create: function (con, data, callback){
+    con.query(`
+      INSERT INTO order
+      SET customer_id='${data.custID}',
+          branch_id='${data.branchID}',
+          user_id='${data.userID}',
+          package_id='${data.packageID}',
+          kode='${data.kode}',
+          jenis_marketing='${data.jenisMarketing}',
+          jenis_pembayaran='${data.jenisPembayaran}',
+          tanggal='${data.tanggal}'`,
+      callback
+    )
+  },
+
+  
 }
