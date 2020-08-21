f151c1607775659fb00ce86156f24bb874b06594
rstx32 <rstx32@gmail.com>
2020-08-22 03:36:38 +0700

    delete route folder

--- routes/orderRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const orderController = require("../controller/orderController")
-
-router.get("/", orderController.index)
-router.get("/create")
-
-module.exports = router
