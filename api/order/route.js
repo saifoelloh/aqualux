171473767e33d75e00380a8ce80e2b7c0718a252
rstx32 <rstx32@gmail.com>
2020-09-06 00:41:35 +0700

    tambah validasi

--- api/order/route.js
+++ api/order/route.js
@@ -1,11 +1,23 @@
 const router = require('express').Router()
 const controller = require('./controller')
+const {check,validationResult} = require('express-validator')
 
 router
   .get('/', controller.index)
   .get('/create', controller.create)
   .get('/:id/edit', controller.edit)
-  .post('/', controller.store)
+  .post('/', [
+    check('jenis_marketing')
+      .notEmpty().withMessage('data tidak boleh kosong'),
+    check('jenis_pembayaran')
+      .notEmpty().withMessage('data tidak boleh kosong')
+  ], (req,res) => {
+    const errors = validationResult(req).array()
+    if (errors)
+      return res.status(400).json({ errors: errors })
+    else
+      controller.store(req,res)
+  })
   .put('/:id', controller.update)
   .delete('/:id', controller.destroy)
 
