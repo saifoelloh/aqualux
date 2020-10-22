0b08154013583bb2358e0b496078c3fe020c6924
rstx32 <rstx32@gmail.com>
2020-10-22 19:05:12 +0700

    tambah search + pagination

--- api/kecamatan/controller.js
+++ api/kecamatan/controller.js
@@ -1,19 +1,48 @@
 const kecamatan = require('./model')
-const { successResponses, errorResponses, pagination } = require('../../utils')
+const { successResponses, errorResponses } = require('../../utils')
+const { Sequelize } = require('sequelize')
+const DatabaseConnection = require('../../config/database')
+
 
 module.exports = {
   getAll: async(req, res) => {
+    const {
+      search = '',
+      show = 10,
+      page = 1,
+      orderBy = 'nama',
+      sortBy = 'ASC',
+    } = req.query
+
     try{
-      const kecamatans = await kecamatan.findAll({
+      let kecamatans = await kecamatan.findAll({
+        order: Sequelize.literal(`${orderBy} ${sortBy}`),
+        offset: show * page,
+        limit: show,
         attributes: {
           exclude: 'kabupatenId'
         },
         include: 'kabupaten'
       })
-      const data = pagination(kecamatans, {...req.query})
-      return successResponses[200](res, {data})
+      
+      if(search != ''){
+        kecamatans = await DatabaseConnection.query(
+          `SELECT * FROM kecamatans WHERE MATCH(nama) AGAINST(:keyword IN BOOLEAN MODE) ORDER BY ${orderBy} ${sortBy} LIMIT :page, :show`,
+          {
+            model: kecamatan,
+            type: Sequelize.QueryTypes.SELECT,
+            replacements: {
+              keyword: `*${search}*`,
+              page: page * show,
+              show: show,
+            },
+          },
+        )
+      }
+
+      return successResponses[200](res, {data: kecamatans})
     }catch(err){
       return errorResponses[400](res, {message: err.message})
     }
-  }
+  },
 }
