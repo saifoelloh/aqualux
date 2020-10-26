898ac41ea36c8e6c28e18b43ba42915d78cd7362
saifoelloh <saifoelloh@gmail.com>
2020-10-26 15:04:06 +0700

    rename validation variable name

--- api/user/schema.js
+++ api/user/schema.js
@@ -1,11 +1,11 @@
 const Joi = require('joi')
 
 const schema = Joi.object().keys({
-    nama: Joi.string().required(),
-    jabatan: Joi.string().required(),
-    telepon: Joi.string().alphanum().min(10).required(),
-    email: Joi.string().email().required(),
-    addressId: Joi.number().integer().required(),
+  nama: Joi.string().required(),
+  jabatan: Joi.string().required(),
+  telepon: Joi.string().alphanum().min(10).required(),
+  email: Joi.string().email().required(),
+  addressesId: Joi.number().integer().required(),
 })
 
 module.exports = schema
